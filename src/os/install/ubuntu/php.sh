e9df81ab930edfb1a28834afc669a40e1bbf9cfe
Johan Alvarez <llstarscreamll@hotmail.com>
2021-03-27 10:22:36 -0500

    Add PHP extensions

--- src/os/install/ubuntu/php.sh
+++ src/os/install/ubuntu/php.sh
@@ -20,8 +20,8 @@ if ! package_is_installed "php7.4"; then
 
 fi
 
-install_package "PHP 7.3" "php7.3 php7.3-mbstring php7.3-intl php7.3-xml php7.3-curl php7.3-zip php7.3-sqlite php7.3-mysql php7.3-redis php7.3-bcmath"
-install_package "PHP 7.4" "php7.4 php7.4-mbstring php7.4-intl php7.4-xml php7.4-curl php7.4-zip php7.4-sqlite php7.4-mysql php7.4-redis php7.4-bcmath php7.4-gd"
+install_package "PHP 7.3" "php7.3 php7.3-mbstring php7.3-intl php7.3-xml php7.3-curl php7.3-zip php7.3-sqlite php7.3-mysql php7.3-redis php7.3-bcmath php7.3-imagick"
+install_package "PHP 7.4" "php7.4 php7.4-mbstring php7.4-intl php7.4-xml php7.4-curl php7.4-zip php7.4-sqlite php7.4-mysql php7.4-redis php7.4-bcmath php7.4-imagick php7.4-gd"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
